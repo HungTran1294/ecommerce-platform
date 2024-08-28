7cf5bde6b4d47afafdf68d00c446aada583e13d7
HungTran <tranquochung2712@gmail.com>
2024-08-28 17:51:09 +0700

    add firebase congif

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1aAVaV1tPCjaVvnF0z4LWinj2i50d18M",
+    authDomain: "ecommerce-platform-d2bfc.firebaseapp.com",
+    projectId: "ecommerce-platform-d2bfc",
+    storageBucket: "ecommerce-platform-d2bfc.appspot.com",
+    messagingSenderId: "933502496103",
+    appId: "1:933502496103:web:a9dbd0cd58076f6c0ed3c2",
+    measurementId: "G-2TBTZ9K832"
+  };
+  export default firebaseConfig;
